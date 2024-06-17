0f7563d8c31039415169063a6ea1a233ed771c9f
Gabriel <155983589+Gabypic@users.noreply.github.com>
2024-06-17 09:29:22 +0200

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,19 +1,25 @@
-import os
 import subprocess
 import datetime
 
-SOURCE = "/home/gaby/Desktop/to_save"
-DEST = "./saves"
-SERVER = "gaby@10.0.2.15"
+SOURCE = "/home/bots/projetsauvegarde/toSave"
+DEST = "/home/bots/projetsauvegarde/saves"
+SERVER = "bots@185.132.47.110"
 
-rsync_command = f"rsync -avz --delete {SOURCE} {SERVER}:{DEST}"
-result = subprocess.run(rsync_command, shell=True)
+rsync_command = f"rsync -avz --delete -e 'ssh -i /chemin/vers/votre/clé/privée/id_rsa' {SOURCE} {SERVER}:{DEST}"
+result_rsync = subprocess.run(rsync_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-now = datetime.datetime.now().strftime('%Y-%m-%d')
-borg_command = f"borg create --stats --progress {SERVER}:/home/gaby/Desktop/ProjetSauvegarde/server/backup::{now} {SOURCE}"
-result = subprocess.run(borg_command, shell=True)
-
-if result.returncode == 0:
-    print("Sauvegarde réussie")
+if result_rsync.returncode == 0:
+    print("Rsync - Sauvegarde réussie")
 else:
-    print("Échec de la sauvegarde")
+    print("Rsync - Échec de la sauvegarde")
+    print(result_rsync.stderr.decode('utf-8'))
+
+now = datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
+borg_command = f"borg create --stats --progress -e 'ssh -i /chemin/vers/votre/clé/privée/id_rsa' {SERVER}:/home/bots/projetsauvegarde/backup::{now} {SOURCE}"
+result_borg = subprocess.run(borg_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+if result_borg.returncode == 0:
+    print("Borg - Sauvegarde réussie")
+else:
+    print("Borg - Échec de la sauvegarde")
+    print(result_borg.stderr.decode('utf-8'))
